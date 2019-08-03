1d45127fc352f50cf36b16196446ef28b139e3f4
liyufei <liyufei@xingyunfenqi.com>
2019-08-03 11:38:11 +0800

    fix: 注释掉auth连接数据库代码

--- auth-server/src/main/java/com/code/auth/config/OAuth2AuthorizationServerConfig.java
+++ auth-server/src/main/java/com/code/auth/config/OAuth2AuthorizationServerConfig.java
@@ -3,9 +3,12 @@ package com.code.auth.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
 @EnableAuthorizationServer
-public class CustomAuthorizationServerConfig {
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
 }
--- auth-server/src/main/java/com/code/auth/model/UserInfo.java
+++ auth-server/src/main/java/com/code/auth/model/UserInfo.java
@@ -1,20 +1,20 @@
-package com.code.auth.model;
-
-import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Data
-@Entity
-public class UserInfo {
-
-    @Id
-    private Long id;
-
-    private String username;
-
-    private String password;
-
-    private String phone;
-}
+// package com.code.auth.model;
+//
+// import lombok.Data;
+//
+// import javax.persistence.Entity;
+// import javax.persistence.Id;
+//
+// @Data
+// @Entity
+// public class UserInfo {
+//
+//     @Id
+//     private Long id;
+//
+//     private String username;
+//
+//     private String password;
+//
+//     private String phone;
+// }
