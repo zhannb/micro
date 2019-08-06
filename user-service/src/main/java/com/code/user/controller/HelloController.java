7c8a03ddad741002728cf99d8a7073c2f850c2a1
liuliang <liuliang@meifute.com>
2019-08-06 19:38:02 +0800

    修改config

--- user-service/src/main/java/com/code/user/controller/HelloController.java
+++ user-service/src/main/java/com/code/user/controller/HelloController.java
@@ -5,12 +5,14 @@ import com.code.user.util.MicroUserUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class HelloController {
 
     @Autowired
