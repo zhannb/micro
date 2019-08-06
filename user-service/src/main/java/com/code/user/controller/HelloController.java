55bc77db2380aedda0abd8ab73bd9cdea766a8cf
liuliang <liuliang@meifute.com>
2019-08-06 16:14:43 +0800

    添加config

--- user-service/src/main/java/com/code/user/controller/HelloController.java
+++ user-service/src/main/java/com/code/user/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.code.user.feign.OrderClient;
 import com.code.user.util.MicroUserUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,22 @@ public class HelloController {
     @Autowired
     private OrderClient orderClient;
 
+    @Value("${crawler.test}")
+    private String crawler;
+
     @GetMapping("/hello")
     public String hello(@RequestParam("name") String name) {
         System.out.println("入参：" + name);
         log.info("current:{}", MicroUserUtil.getCurrentUser());
         return "hello " + name + "=====" + orderClient.order(name);
     }
+
+
+    @GetMapping("/crawler")
+    public String crawler(){
+        return this.crawler;
+    }
+
+
+
 }
