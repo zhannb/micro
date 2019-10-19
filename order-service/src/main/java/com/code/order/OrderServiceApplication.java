54b539c16a70188b99a9e4850c72cc08bcc473f2
liuliang <liuliang@meifute.com>
2019-10-19 15:40:09 +0800

    添加代码

--- order-service/src/main/java/com/code/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/code/order/OrderServiceApplication.java
@@ -1,9 +1,11 @@
 package com.code.order;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDistributedTransaction
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
